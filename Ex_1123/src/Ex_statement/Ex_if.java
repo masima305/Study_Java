70f596b04336c17bbaccd1912ded4ed70aca5c46
JeongHoon <masima305@gmail.com>
2017-11-23 11:45:13 +0900

    man

---
+++ Ex_1123/src/Ex_statement/Ex_if.java
@@ -0,0 +1,19 @@
+package Ex_statement;
+
+public class Ex_if {
+	public static void main(String[] args) {
+		// 제어문
+		// 프로그램의 흐름을 제어하는 문장
+		//분기문과 반복문으로 나뉜다.
+		
+		// 분기문 : if, switch
+		// 반복문 : for, while, do-while
+		
+		//단순 if문
+		int n = 50;
+		String str = "안녕하세요";
+		if (n == 50) {
+			str = "n은 50입니다.";
+		}
+	}
+}
